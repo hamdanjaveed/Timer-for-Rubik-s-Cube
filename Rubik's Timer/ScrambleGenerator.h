b47fc3d73e69ae7cf6d929932bcabce08c256769
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-12-25 22:32:40 -0500

    Added a scramble generator

---
+++ Rubik's Timer/ScrambleGenerator.h
@@ -0,0 +1,15 @@
+//
+//  ScrambleGenerator.h
+//  Rubik's Timer
+//
+//  Created by Hamdan Javeed on 12/25/2013.
+//  Copyright (c) 2013 Hamdan Javeed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ScrambleGenerator : NSObject
+
++ (NSString *)generateScramble;
+
+@end
---
+++ Rubik's Timer/ScrambleGenerator.m
@@ -0,0 +1,34 @@
+//
+//  ScrambleGenerator.m
+//  Rubik's Timer
+//
+//  Created by Hamdan Javeed on 12/25/2013.
+//  Copyright (c) 2013 Hamdan Javeed. All rights reserved.
+//
+
+#import "ScrambleGenerator.h"
+
+@implementation ScrambleGenerator
+
++ (NSString *)generateScramble {
+    NSArray *possibleMoves = @[@"F", @"R", @"U", @"L", @"B", @"D", @"F'", @"R'", @"U'", @"L'", @"B'", @"D'", @"F2", @"R2", @"U2", @"L2", @"B2", @"D2"];
+    int scrambleLength = 25;
+    NSMutableArray *scrambleArray = [NSMutableArray arrayWithObject: [NSNumber numberWithInt:(arc4random() % [possibleMoves count])]];
+    for (int i = 1; i < scrambleLength; i++) {
+        int nextMove;
+        do {
+            nextMove = arc4random() % [possibleMoves count];
+        } while (nextMove == [scrambleArray[i - 1] integerValue]);
+        
+        scrambleArray[i] = [NSNumber numberWithInt:nextMove];
+    }
+    
+    NSMutableArray *scrambleArrayMoves = [[NSMutableArray alloc] init];
+    for (int i = 0; i < scrambleLength; i++) {
+        scrambleArrayMoves[i] = possibleMoves[[scrambleArray[i] integerValue]];
+    }
+    
+    return [scrambleArrayMoves componentsJoinedByString:@"  "];
+}
+
+@end
