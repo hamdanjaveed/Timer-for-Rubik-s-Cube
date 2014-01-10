0aa32a6fe78641d843243ae8a47a990157f1307f
Hamdan Javeed <hamdan@hamdanjaveed.com>
2014-01-09 23:38:06 -0500

    Tweaked UI

--- Rubik's Timer/RubiksTimeViewController.m
+++ Rubik's Timer/RubiksTimeViewController.m
@@ -24,15 +24,6 @@
 
 @property (weak, nonatomic) IBOutlet UILabel *scrambleLabel;
 
-@property (strong, nonatomic) UIDynamicAnimator *animator;
-@property (strong, nonatomic) UISnapBehavior *snap1;
-@property (strong, nonatomic) UISnapBehavior *snap2;
-
-@property (strong, nonatomic) UISnapBehavior *snap3;
-@property (strong, nonatomic) UISnapBehavior *snap4;
-
-@property (strong, nonatomic) UIDynamicItemBehavior *snapItem;
-
 @property (weak, nonatomic) IBOutlet UIView *blurView;
 @property (nonatomic) BOOL isBlurred;
 @end
@@ -43,26 +34,6 @@
     [super viewDidLoad];
     self.fireDate = [NSDate date];
     [self generateScramble];
-    
-    self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
-    self.snap1 = [[UISnapBehavior alloc] initWithItem:self.scrambleLabel snapToPoint:CGPointMake(self.view.bounds.size.width / 2, self.view.bounds.size.height - 140)];
-    self.snap2 = [[UISnapBehavior alloc] initWithItem:self.scrambleLabel snapToPoint:CGPointMake(self.view.bounds.size.width / 2, self.view.bounds.size.height)];
-    [self.animator addBehavior:self.snap1];
-    
-    self.snap3 = [[UISnapBehavior alloc] initWithItem:self.timerLabel snapToPoint:CGPointMake(self.view.bounds.size.width / 2, self.view.bounds.size.height - 340)];
-    self.snap4 = [[UISnapBehavior alloc] initWithItem:self.timerLabel snapToPoint:CGPointMake(self.view.bounds.size.width / 2, self.view.bounds.size.height - 300)];
-    [self.animator addBehavior:self.snap3];
-    
-    double damp = 0.3;
-    self.snap1.damping = damp;
-    self.snap2.damping = damp;
-    self.snap3.damping = damp;
-    self.snap4.damping = damp;
-    
-    self.snapItem = [[UIDynamicItemBehavior alloc] initWithItems:@[self.scrambleLabel, self.timerLabel]];
-    self.snapItem.allowsRotation = NO;
-    self.snapItem.resistance = 25;
-    [self.animator addBehavior:self.snapItem];
     
     [self.blurView setOpaque:NO];
     [self.blurView setUserInteractionEnabled:NO];
@@ -92,11 +63,6 @@
             self.timerLabel.text = @"Tap to start";
         }
         
-//        [self.animator removeBehavior:self.snap2];
-//        [self.animator addBehavior:self.snap1];
-//        [self.animator removeBehavior:self.snap4];
-//        [self.animator addBehavior:self.snap3];
-        
         [self toggleBlur];
     }
 }
@@ -109,11 +75,6 @@
         self.inspectionTimer = [NSTimer scheduledTimerWithTimeInterval:0 target:self selector:@selector(updateInspectionTimer:) userInfo:nil repeats:YES];
         [self.inspectionTimer fire];
         self.inspectionDidFinish = NO;
-        
-//        [self.animator removeBehavior:self.snap1];
-//        [self.animator addBehavior:self.snap2];
-//        [self.animator removeBehavior:self.snap3];
-//        [self.animator addBehavior:self.snap4];
         
         [self toggleBlur];
     }
