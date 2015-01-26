6133f7f396d2ed3d2eb28bc9107f7be27845bad5
Hamdan Javeed <hamdan@hamdanjaveed.com>
2015-01-25 19:23:34 -0500

    Square one

---
+++ Rubik's Timer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Rubik's Timer
+//
+//  Created by Hamdan Javeed on 2015-01-25.
+//  Copyright (c) 2015 Hamdan Javeed. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
