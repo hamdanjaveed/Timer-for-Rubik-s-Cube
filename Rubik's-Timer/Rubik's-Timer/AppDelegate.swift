18eae0213297cbd1c8e62f21df7f5524e914db5e
Hamdan Javeed <hamdan@hamdanjaveed.com>
2018-07-15 22:44:46 -0400

    Add some convinience vars for core data

--- Rubik's-Timer/Rubik's-Timer/AppDelegate.swift
+++ Rubik's-Timer/Rubik's-Timer/AppDelegate.swift
@@ -72,6 +72,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return container
     }()
 
+    static var persistentContainer: NSPersistentContainer {
+        return (UIApplication.shared.delegate as! AppDelegate).persistentContainer
+    }
+
+    static var viewContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+
     // MARK: - Core Data Saving support
 
     func saveContext () {
