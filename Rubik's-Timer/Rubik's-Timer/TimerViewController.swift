0c2cf67186717b043931928ac0113f9703199cc2
Hamdan Javeed <hamdan@hamdanjaveed.com>
2018-07-18 15:09:21 -0400

    Add a timer and solves view controller

---
+++ Rubik's-Timer/Rubik's-Timer/TimerViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TimerViewController.swift
+//  Rubik's-Timer
+//
+//  Created by Hamdan Javeed on 2018-07-18.
+//  Copyright © 2018 Hamdan Javeed. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+
+}
